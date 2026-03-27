e1bb8769bccebb2a6931a7d8b73da07e498dea69
dt764 <dt764@inlumine.ual.es>
2026-03-27 19:16:57 +0100

    Estilizado  pagina principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonCard } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   standalone: true,
-  imports: [IonContent, IonButton, RouterLink],
+  imports: [IonContent, IonButton, RouterLink, IonCard],
 })
 export class HomePage {}
